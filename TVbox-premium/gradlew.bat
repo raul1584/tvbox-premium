45d8210c05549702c91ec08089873b27ad0821ff
raul1584 <weopweop969@gmail.com>
2026-09-16 15:56:03 -0300

    Create gradlew.bat

---
+++ TVbox-premium/gradlew.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+SET DIR=%~dp0
+SET APP_HOME=%DIR%
+
+IF DEFINED JAVA_HOME (
+    SET JAVA_EXE=%JAVA_HOME%\bin\java.exe
+) ELSE (
+    SET JAVA_EXE=java.exe
+)
+
+"%JAVA_EXE%" -Dorg.gradle.appname=gradlew -classpath "%APP_HOME%gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
