447d4235ae128a4153605a16608c9708bf694ba7
raul1584 <weopweop969@gmail.com>
2026-09-16 16:28:13 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,13 @@ android {
 }
 
 dependencies {
+    // Componentes base de Android
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+
+    // Media3 / ExoPlayer (Tus dependencias originales + extractores para streams IPTV)
     implementation("androidx.media3:media3-exoplayer:1.5.1")
     implementation("androidx.media3:media3-ui:1.5.1")
     implementation("androidx.media3:media3-exoplayer-hls:1.5.1")
+    implementation("androidx.media3:media3-extractor:1.5.1")
 }
