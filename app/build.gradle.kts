110001f7c44a33cbe397466e21d4fdcd65a40453
raul1584 <weopweop969@gmail.com>
2026-09-16 14:54:38 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,3 +30,9 @@ android {
         jvmTarget = "17"
     }
 }
+
+dependencies {
+    implementation("androidx.media3:media3-exoplayer:1.5.1")
+    implementation("androidx.media3:media3-ui:1.5.1")
+    implementation("androidx.media3:media3-exoplayer-hls:1.5.1")
+}
